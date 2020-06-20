95938b0a801fde9ff949fca65b172dcb7a479693
ayushgoyal7796 <ayushgoyal7796@gmail.com>
2020-06-20 22:46:54 +0530

    add provider package

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
+import 'package:brewcrew/models/user.dart';
 import 'package:brewcrew/screens/wrapper.dart';
+import 'package:brewcrew/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Brew Crew',
-      home: Wrapper(),
+    return StreamProvider<User>.value(
+      value: Auth().user,
+      child: MaterialApp(
+        title: 'Brew Crew',
+        home: Wrapper(),
+      ),
     );
   }
 }
