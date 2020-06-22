8f8d66c40f4f400106034acb66b2083ea8b0dd82
ayushgoyal7796 <ayushgoyal7796@gmail.com>
2020-06-22 07:10:54 +0530

    update database

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,7 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
+  final String uid;
+
+  DatabaseService({this.uid});
+
   // collection reference
   final CollectionReference brewCollection =
       Firestore.instance.collection('brews');
+
+  Future updateUserData(String name, String sugars, int strength) async {
+    return await brewCollection.document(uid).setData({
+      'name': name,
+      'sugars': sugars,
+      'strength': strength,
+    });
+  }
 }
